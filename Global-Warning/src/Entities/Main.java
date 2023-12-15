51df3a789036510da5fe41866efc672db8a4839d
NotBip <hamadmohamed0009@gmail.com>
2023-12-15 14:00:42 -0500

    Made a few files

---
+++ Global-Warning/src/Entities/Main.java
@@ -0,0 +1,16 @@
+package Entities;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
+//FAILURE - bobby
+// i am testing things
+//I wanna cry
+//dont cry bobby, remember that the new bardock-goku tag in dbl releasedAND I GOT IT
+// And that megaman drops January 7th?
+//OOOH 
+//LISTEN!!!
