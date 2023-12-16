6100bdf0d15b68b10cc26fc5edfb2d124ccdd440
NotBip <hamadmohamed0009@gmail.com>
2023-12-16 17:00:22 -0500

    Bas3

--- Global-Warning/src/Main/Main.java
+++ Global-Warning/src/Main/Main.java
@@ -1,7 +1,12 @@
 package Main;
+import hsa2.*;
+import java.awt.Color;
+
 
 public class Main {
+
+    private static GraphicsConsole gc = new GraphicsConsole(500, 500); 
     public static void main(String[] args) {
-        
+        gc.setBackgroundColor(Color.GRAY);
     }
 }
