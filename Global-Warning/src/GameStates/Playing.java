f9ff91b2bc5fb2192ada660dd0f7e2aed5f4183a
Hamou <zeldahamou@gmail.com>
2023-12-18 16:48:42 -0500

    Weapon as extention of player

--- Global-Warning/src/GameStates/Playing.java
+++ Global-Warning/src/GameStates/Playing.java
@@ -1,6 +1,7 @@
 package GameStates;
 
 import Entities.*;
+import Objects.Weapons.Weapon1;
 import Levels.LevelManager;
 import Main.Game;
 import Objects.ObjectManager;
@@ -12,6 +13,7 @@ import java.awt.event.*;
 
 public class Playing extends State implements KeyListener{
     private Player player;
+    private Weapon1 weapon;
     private LevelManager levelManager;
     private ObjectManager objectManager;
     private boolean paused;
@@ -29,10 +31,12 @@ public class Playing extends State implements KeyListener{
 
     public void initialize() {
         player = new Player(10, GAME_HEIGHT-100, 60, 80);
+        weapon = new Weapon1(10, GAME_HEIGHT-100, 20, 40);
     }
 
     public void update() {
         player.update();
+        weapon.update();
     }
 
     public void checkBorder() {
@@ -41,12 +45,18 @@ public class Playing extends State implements KeyListener{
 
     public void draw(Graphics g) {
             player.draw(g);
+            weapon.draw(g);
         
     }
 
     public void reset() {
 
     }
+
+     public Weapon1 getWeapon1() {
+        return weapon;
+    }
+
 
     public Player getPlayer() {
         return player;
@@ -65,18 +75,23 @@ public class Playing extends State implements KeyListener{
         switch(e.getKeyCode()) {
             case KeyEvent.VK_A:
             player.setLeft(true);
+            weapon.setLeft(true);
             break;
             case KeyEvent.VK_D:
             player.setRight(true);
+            weapon.setRight(true);
             break;
             case KeyEvent.VK_W:
             player.setUp(true);
+            weapon.setUp(true);
             break;
             case KeyEvent.VK_S:
             player.setDown(true);
+            weapon.setDown(true);
             break;
             case KeyEvent.VK_SPACE:
             player.jump();
+            weapon.jump();
             break;
         }
     }
@@ -85,15 +100,19 @@ public class Playing extends State implements KeyListener{
         switch(e.getKeyCode()) {
             case KeyEvent.VK_A:
             player.setLeft(false);
+            weapon.setLeft(false);
             break;
             case KeyEvent.VK_D:
             player.setRight(false);
+            weapon.setRight(false);
             break;
             case KeyEvent.VK_W:
             player.setUp(false);
+            weapon.setUp(false);
             break;
             case KeyEvent.VK_S:
             player.setDown(false);
+            weapon.setDown(false);
             break;
         }
     }
--- Global-Warning/src/Objects/Weapons/Weapon1.java
+++ Global-Warning/src/Objects/Weapons/Weapon1.java
@@ -1,13 +1,37 @@
 package Objects.Weapons;
 
-public class Weapon1 {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+import Entities.Entity;
+
+public class Weapon1 extends Entities.Player{
+   //entends entity for hitbox stuffs
+
+
+   //private MouseInputs mouseInputs;
    private int bulletDamage = 1;
    private int bulletAmount = 1;
    private double bulletRange = 5.5;
    private double bulletSpeed = 2.2;
    private boolean canShoot = false;
-   //bulletSize is a hitbox, ish, kinda things with dimensions
+   protected Rectangle2D.Float gunBox;
 
    
 
+   //bulletSize is a hitbox, ish, kinda things with dimensions
+
+   public Weapon1(float x, float y, int width, int height) {
+     super(x, y, width, height);
+      initialize();
+  }
+
+  public void draw(Graphics g) {
+   drawHitbox(g);
 }
+
+
+
+   
+}
+
