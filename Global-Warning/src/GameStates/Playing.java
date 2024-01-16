d5e0db3073f18d725ac6fca94800280bdd3cfa57
NotBip <hamadmohamed0009@gmail.com>
2024-01-16 13:45:27 -0500

    asd

--- Global-Warning/src/GameStates/Playing.java
+++ Global-Warning/src/GameStates/Playing.java
@@ -118,6 +118,7 @@ public class Playing extends State implements KeyListener, MouseListener {
     }
 
     public void update() throws IOException {
+        System.out.println(this.getLevelManager().getCurrentLevel().getStormy());
         if (paused) {
 			pauseScreen.update();
 		} else if (inventory && !paused){
