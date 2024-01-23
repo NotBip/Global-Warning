d45a9c31a0b61d8c81e594e3a58a58d8aa80deae
NotBip <hamadmohamed0009@gmail.com>
2024-01-22 23:06:13 -0500

    savet the change

--- Global-Warning/src/GameStates/Menu.java
+++ Global-Warning/src/GameStates/Menu.java
@@ -105,7 +105,6 @@ public class Menu extends State implements KeyListener, MouseListener {
 
         g.drawImage(this.imgbackground, 0, 0, this.width, this.height, null);
         g.drawImage(this.imgtitle, GAME_WIDTH / 3 + 40, 0, 350, 200, null);
-        g.drawImage(this.teachControls, GAME_WIDTH - 350, 40, 300, 400, null);
         for (MenuButton mb : buttons)
             mb.draw(g);
 
